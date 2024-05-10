4d152a3993942a75ff77d2fa7b71251d16e106a0
saifulaija <saifulaija2023@gmail.com>
2024-05-10 19:05:25 +0600

    change password done

---
+++ src/app/(withDashboardLayout)/dashboard/change-password/page.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
+import { FieldValues } from "react-hook-form";
+import { z } from "zod";
+import KeyIcon from "@mui/icons-material/Key";
+
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import BCForm from "@/components/Forms/BCForm";
+import BCInput from "@/components/Forms/BCInput";
+import { useChangePasswordMutation } from "@/redux/api/authApi";
+
+const validationSchema = z.object({
+  oldPassword: z.string().min(6, "Must be at least 6 characters long"),
+  newPassword: z.string().min(6, "Must be at least 6 characters long"),
+});
+
+const ChangePassword = () => {
+  const router = useRouter();
+  const [changePassword] = useChangePasswordMutation();
+
+  const onSubmit = async (values: FieldValues) => {
+    try {
+      const res = await changePassword(values);
+      if (res?.data?.status === 200) {
+        toast.success("password changed successfully!!");
+      } else {
+        throw new Error("Incorrect old password");
+      }
+    } catch (err:any) {
+      toast.success("Incorrect Old Password");
+      console.log(err);
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        px: 4,
+        py: 2,
+        maxWidth: 600,
+        width: "100%",
+        boxShadow: 1,
+        borderRadius: 1,
+        mx: "auto",
+        mt: {
+          xs: 2,
+          md: 5,
+        },
+      }}
+    >
+      <Stack alignItems="center" justifyContent="center">
+        <Box
+          sx={{
+            "& svg": {
+              width: 100,
+              height: 100,
+            },
+          }}
+        >
+          <KeyIcon sx={{ color: "primary.main" }} />
+        </Box>
+        <Typography variant="h5" fontWeight={600} sx={{ mb: 2, mt: -1.5 }}>
+          Change password
+        </Typography>
+      </Stack>
+      <BCForm
+        onSubmit={onSubmit}
+        defaultValues={{ oldPassword: "", newPassword: "" }}
+        resolver={zodResolver(validationSchema)}
+      >
+        <Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <BCInput
+              name="oldPassword"
+              type="password"
+              label="Old Password"
+              fullWidth
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <BCInput
+              name="newPassword"
+              type="password"
+              label="New Password"
+              fullWidth
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+        </Grid>
+
+        <Button type="submit" sx={{ width: "100%", my: 2 }}>
+          change Password
+        </Button>
+      </BCForm>
+    </Box>
+  );
+};
+
+export default ChangePassword;
